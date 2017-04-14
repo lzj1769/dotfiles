87a209bca1849a71d4c7e54c76a6b77ee3e7f7e7
Alex Hunt <hello@alexhunt.io>
2017-04-14 13:05:09 +0100

    Fix non-printing escape sequences in PS1 prompt

--- bash/.bash/prompts.sh
+++ bash/.bash/prompts.sh
@@ -5,7 +5,7 @@ export PROMPT_COMMAND='echo -ne "\033]0;$([ "$PWD" == "$HOME" ] && echo "$PWD" |
 if [ -f ~/.git-prompt.sh ]; then
     source ~/.git-prompt.sh
 
-    export PS1='\e[38;5;240m\]\u \w$(__git_ps1)\e[0;0m\] \$ '
+    export PS1='\[\e[38;5;240m\]\u \w$(__git_ps1)\[\e[0;0m\] \$ '
 else
-    export PS1='\e[38;5;240m\]\u \w\e[0;0m\] \$ '
+    export PS1='\[\e[38;5;240m\]\u \w\[\e[0;0m\] \$ '
 fi
