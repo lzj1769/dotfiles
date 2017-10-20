e95eafdb92e79b43a81d22a5ad0fdc4e590a57a5
Alex Hunt <hello@alexhunt.io>
2017-10-20 14:47:22 +0100

    Set default search command for fzf

--- bash/.bash/exports.sh
+++ bash/.bash/exports.sh
@@ -2,5 +2,12 @@ export EDITOR=vim
 export HISTFILESIZE=
 export HISTSIZE=
 export HISTTIMEFORMAT='[%F %T] '
+
+# Repaint screen upon exiting less
 export LESS=R
+
+# Highlight color for grep matches
 export GREP_COLOR='1;32'
+
+# Perform fzf searches using ag, with options
+export FZF_DEFAULT_COMMAND='ag --hidden --depth=10 --ignore .git -f -g ""'
