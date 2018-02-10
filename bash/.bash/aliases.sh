b05f1ea1cebbd6b9f4e77c59bff05b081e8b1cb8
Alex Hunt <hello@alexhunt.io>
2018-02-10 11:27:30 +0000

    Update developer SDK path entries

--- bash/.bash/aliases.sh
+++ bash/.bash/aliases.sh
@@ -5,6 +5,7 @@ alias diff=colordiff
 alias grep='grep -n --color'
 alias npmlist='npm list -g --depth=0'
 alias tinker='php artisan tinker'
+alias emulator="$ANDROID_HOME/tools/emulator"
 alias mjson='python -mjson.tool'
 alias rand='openssl rand -base64'
 alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
--- bash/.profile
+++ bash/.profile
@@ -1,3 +1,9 @@
+# Android SDK
+export ANDROID_HOME=~/Android/Sdk
+
 # PATH
-export PATH=$HOME/bin:$HOME/.local/bin:$PATH
+export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/.composer/vendor/bin:$PATH
+export PATH=$ANDROID_HOME/tools:$PATH
+
+export CPATH=/usr/include $CPATH
