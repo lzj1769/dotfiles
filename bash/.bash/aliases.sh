54fb64162e3fc4a84044a4e8ab27b3a3fc42b8c0
Alex Hunt <hello@alexhunt.io>
2018-02-19 10:45:54 +0000

    Add setclip and getclip command aliases

--- bash/.bash/aliases.sh
+++ bash/.bash/aliases.sh
@@ -1,8 +1,13 @@
 alias ..='cd ..'
 alias ls='ls -F -h'
+
+alias setclip='xclip -selection c'
+alias getclip='xclip -selection c -o'
+
 alias rsync='rsync --progress'
 alias diff=colordiff
 alias grep='grep -n --color'
+
 alias npmlist='npm list -g --depth=0'
 alias tinker='php artisan tinker'
 alias emulator="$ANDROID_HOME/tools/emulator"
@@ -11,5 +16,7 @@ alias rand='openssl rand -base64'
 alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
 
 if [[ "$OSTYPE" = "darwin"* ]]; then
+    alias setclip=pbcopy
+    alias getclip=pbpaste
     alias tmux='tmux -f ~/.tmux-macos.conf'
 fi
