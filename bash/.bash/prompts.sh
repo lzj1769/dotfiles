33183f112a76249b91cfad9a235e2face8b7bbf5
Alex Hunt <hello@alexhunt.io>
2018-11-19 14:27:48 +0000

    Fix warning when locating .git-prompt.sh

--- bash/.bash/prompts.sh
+++ bash/.bash/prompts.sh
@@ -2,8 +2,8 @@
 export PROMPT_COMMAND='echo -ne "\033]0;$([ "$PWD" == "$HOME" ] && echo "$PWD" || echo "${PWD/#$HOME/\~}") - $(basename $SHELL)\007"'
 
 # PS1
-if [ -f ~/.git-prompt.sh ]; then
-    source ~/.git-prompt.sh
+if [ -f $HOME/.git-prompt.sh ]; then
+    source $HOME/.git-prompt.sh
 
     export PS1='\[\e[38;5;240m\]\u \w$(__git_ps1)\[\e[0;0m\] \$ '
 else
