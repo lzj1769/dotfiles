a0e546580f08bc2a59b6ce67ed0fef02addab9ea
Alex Hunt <hello@alexhunt.io>
2019-01-17 15:25:27 +0000

    Remove global flag from fish set and abbr calls

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,16 +9,16 @@ end
 
 # - Exports -
 
-set -x -g EDITOR vim
+set -x EDITOR vim
 
 # Repaint screen upon exiting less
-set -x -g LESS r
+set -x LESS r
 
 # Highlight color for grep matches
-set -x -g GREP_COLOR '1;32'
+set -x GREP_COLOR '1;32'
 
 # Perform fzf searches using ag, with options
-set -x -g FZF_DEFAULT_COMMAND 'ag --hidden --depth=10 --ignore .git -f -g ""'
+set -x FZF_DEFAULT_COMMAND 'ag --hidden --depth=10 --ignore .git -f -g ""'
 
 # - Aliases -
 
@@ -29,12 +29,12 @@ alias grep 'grep -n --color'
 alias setclip 'xclip -selection c'
 alias getclip 'xclip -selection c -o'
 
-abbr -a -g rsync 'rsync --progress'
-abbr -a -g npmlist 'npm list -g --depth=0'
-abbr -a -g phpunit './vendor/bin/phpunit --stop-on-fail --stop-on-error'
-abbr -a -g tinker 'php artisan tinker'
-abbr -a -g emulator "$ANDROID_HOME/tools/emulator"
-abbr -a -g rand 'openssl rand -base64'
+abbr -a rsync 'rsync --progress'
+abbr -a npmlist 'npm list -g --depth=0'
+abbr -a phpunit './vendor/bin/phpunit --stop-on-fail --stop-on-error'
+abbr -a tinker 'php artisan tinker'
+abbr -a emulator "$ANDROID_HOME/tools/emulator"
+abbr -a rand 'openssl rand -base64'
 
 if [ (uname) = 'Darwin' ]
     alias diff icdiff
@@ -42,7 +42,7 @@ if [ (uname) = 'Darwin' ]
     alias getclip pbpaste
     alias tmux 'tmux -f ~/.tmux-macos.conf'
 
-    abbr -a -g tower gittower
+    abbr -a tower gittower
 else
     alias open xdg-open
 end
