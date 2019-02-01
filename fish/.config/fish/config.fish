23f9c390c5c6ed31c8b548ac39405bcd53973af1
Alex Hunt <hello@alexhunt.io>
2019-02-01 10:34:45 +0000

    Add Fisher fish bootstrap script and include fish-nvm package

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -46,3 +46,12 @@ if [ (uname) = 'Darwin' ]
 else
     alias open xdg-open
 end
+
+# - Packages -
+
+# Install and run Fisher package manager on first run
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
