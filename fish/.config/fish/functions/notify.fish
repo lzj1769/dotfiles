7dfa5244bcb4348caa2fe5624e2cd047f64e762e
Alex Hunt <hello@alexhunt.io>
2019-09-20 10:07:27 +0100

    fish: Replace notify function with franciscolourenco/done

--- fish/.config/fish/functions/notify.fish
+++
@@ -1,8 +0,0 @@
-function notify --description 'Run a command and send a system notification on completion'
-    if not eval $argv
-        terminal-notifier -message "Failed: $argv" -title 'notify'
-        return
-    end
-
-    terminal-notifier -message "Completed: $argv" -title 'notify'
-end
